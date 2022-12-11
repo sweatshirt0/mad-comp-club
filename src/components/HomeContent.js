a1b3d084971e028791cc57d74343511914b54210
sweatshirt0 <sweatshirttt@proton.me>
2022-12-10 23:05:47 -0500

    slight home page changes.

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -4,7 +4,7 @@ const HomeContent = () => {
 	return(
 		<div class="home-content-wrapper">
 			<h1 class="home-content-title">Mad Computer Club.</h1>
-			<p class="home-content">Welcome.</p>
+			<p class="home-content">Where sanity is just a concept.</p>
 		</div>
 	)
 }
